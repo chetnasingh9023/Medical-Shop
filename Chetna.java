fe114607bb67e3b94c936e9e13fa3fe508747a3f
chetna singh <chetnasingh7488@gmail.com>
2022-07-10 22:08:33 +0530

    initial commit

---
+++ Chetna.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Chetna {
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        int a;
+        int b;
+        int sum;
+        a= scn.nextInt();
+        System.out.println("Enter first number");
+        b = scn.nextInt();
+        System.out.println("Enter second number");
+        sum = scn.nextInt();
+        System.out.println("sum");
+        sum = a+b;
+        System.out.println("sum of these numbers: "+sum);
+    }
+}
